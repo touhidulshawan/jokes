3fafc1af2b3fd912f624dfd5ef6d41a07a0a71b2
Touhidul Shawan <touhidulshawan@gmail.com>
2020-10-29 08:55:33 +0600

    complete the app

--- src/App.tsx
+++ src/App.tsx
@@ -17,23 +17,49 @@ const App: React.FC = () => {
     setup: "What did one wall say to the other wall?",
     punchline: "I'll meet you at the corner!",
   });
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    const getJokes = async () => {
-      const response = await axios.get(
-        "https://official-joke-api.appspot.com/random_joke"
-      );
-      setJokes(response.data);
-    };
-    getJokes();
+    handleNewJokes();
   }, []);
 
-  return (
-    <main>
-      <h1>Jokes</h1>
+  const handleNewJokes = async () => {
+    setLoading(true);
+    const response = await axios.get(
+      "https://official-joke-api.appspot.com/random_joke"
+    );
+    setJokes(response.data);
+    setLoading(false);
+  };
+
+  const renderLoader = <div className={`loader ${spinner}`}></div>;
+
+  const renderJokes = (
+    <div>
       <Jokes jokes={jokes} />
+      <button onClick={handleNewJokes} className={btn}>
+        Next Joke
+      </button>
+    </div>
+  );
+
+  return (
+    <main className={container}>
+      <h1 className={headerStyle}>Jokes</h1>
+      {loading ? renderLoader : renderJokes}
     </main>
   );
 };
 
 export default App;
+
+const container = "w-full h-screen text-center bg-gray-200";
+
+const headerStyle =
+  "block w-screen text-center p-2 mb-8 text-2xl uppercase tracking-wide bg-gray-700 text-gray-200";
+
+const spinner =
+  "w-32 h-32 m-2 m-auto border-8 border-gray-400 rounded-full border-t-8 ";
+
+const btn =
+  "uppercase tracking-wide bg-blue-200 text-blue-700 border-b-8 border-blue-800 px-4 py-2 rounded shadow-md mt-4";
--- src/components/Jokes.tsx
+++ src/components/Jokes.tsx
@@ -7,11 +7,21 @@ interface Props {
 const Jokes: React.FC<Props> = (props) => {
   const { jokes } = props;
   return (
-    <div>
-      <p> -{jokes.setup}</p>
-      <p> - {jokes.punchline}</p>
+    <div className={wrapper}>
+      <p className={textStyle}>
+        <span className={spanStyle}>Joke:</span>
+        {jokes.setup}
+      </p>
+      <p className={textStyle}>
+        <span className={spanStyle}>Punch:</span>
+        {jokes.punchline}
+      </p>
     </div>
   );
 };
 
 export default Jokes;
+const wrapper =
+  "my-2 w-full md:w-2/4 m-auto p-4 border-b-4 border-indigo rounded";
+const textStyle = "text-xl text-gray-700 mb-4";
+const spanStyle = "bg-gray-400 text-gray-7000 mr-2 rounded px-2 py--1";
